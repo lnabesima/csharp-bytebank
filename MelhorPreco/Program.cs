e362a9c80741825666123ccbaa953bc7ddadbd4a
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-15 17:55:58 -0300

    feat: add exercise to render default data

--- MelhorPreco/Program.cs
+++ MelhorPreco/Program.cs
@@ -1,2 +1,9 @@
 ﻿
+using MelhorPreco;
+
 Console.WriteLine("Hello World");
+var pessoa = new Pessoa();
+Console.WriteLine(pessoa.nome);
+Console.WriteLine(pessoa.idade);
+Console.WriteLine(pessoa.altura);
+Console.WriteLine(pessoa.peso);
