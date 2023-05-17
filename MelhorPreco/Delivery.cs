48629b007ca5b8ef367301485d2565bd51548cd7
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-17 18:40:19 -0300

    feat: add 'Delivery.cs' file with 5 new classes

---
+++ MelhorPreco/Delivery.cs
@@ -0,0 +1,40 @@
+﻿namespace MelhorPreco;
+
+public class Delivery
+{
+    public int numeroPedido;
+    public Cliente cliente;
+    public ItemPedido itemPedido;
+    public float precoPedido;
+}
+
+public class Cliente
+{
+    public string nome;
+    public string cpf;
+    public string email;
+    public Endereco enderecoEntrega;
+}
+
+public class Endereco
+{
+    public string logradouro;
+    public int numero;
+    public string cep;
+}
+
+public class ItemPedido
+{
+    public int numeroPedido;
+    public Produto produto1;
+    public Produto produto2;
+    public Produto produto3;
+    public int quantidadePedida;
+}
+
+public class Produto
+{
+    public int codigoProduto;
+    public string descricaoProduto;
+    public double precoProduto;
+}
